b3e7c2ffd8d9c37399b67717fd677f0f3a0febe2
VigneshCBK <vickykasthu@gmail.com>
2022-08-11 22:41:29 +0530

    update.

---
+++ main.js
@@ -0,0 +1,144 @@
+var laptop; {
+    brand: "HP";
+    color: "black";
+    ram: 8;
+    warrenty: "OneYear";
+    shape: "sqaure";
+}
+var mob; {
+    brand: "realme";
+    size: 15.6;
+    color: "black";
+    model: 2;
+    ram: 4;
+}
+var cylinder; {
+    color: "red";
+    shape: "cynilnderical";
+    weight: 30;
+    rate: 1200;
+    type: "steel";
+}
+var pen; {
+    shape: "cylinder";
+    type: "blue";
+    purpose: "writting";
+    rate: 5;
+    length: 15;
+}
+var matchbox; {
+    shape: "sqaure";
+    purpose: "fir"e;
+    type: "wax";
+    size: 5 * 4;
+    rate: 1;
+}
+// =================== 5
+var cooker; {
+    type: "stainless steel";
+    purpose: "cooking";
+    rate: 1500;
+    shape:" cylinder";
+    handle: "plastic";
+}
+var lock;{
+    type: "steel";
+    rate:150;
+    purpose:"lock the room";
+    count of key:2;
+    break-point: "hard";
+}
+var bed;{
+    type:"steel";
+    rate:1500;
+    heighte:6ft;
+    width:4ft;
+    members:1;
+}
+var head-set;{
+    type:"wired";
+    length:1.5m;
+    rate:600rs;
+    ratings:4\5;
+    mic:"clearly heard";
+}
+var ironBox;{
+    brand:"preethi";
+    voltage: 240v;
+    type:"steel";
+    heat:100'c;
+    purpose:"iorining cloths";
+}
+// ==========================10
+var bottle;{
+    type:"steel";
+    capacity:1.5l;
+    heat:100c;
+    damage: "hard";
+    rate:300rs;
+}
+var bus;{
+    brand:"ashok leyland";
+    color:red;
+    load:"48-members";
+    speed:"50kmph";
+    rate:"10lak";
+}
+var bag;{
+    brand:"roshan";
+    rate:"1400rs";
+    capacity:"20kg";
+    warrenty:"1yr";
+    color:"black";
+}
+var note;{
+    color:"white";
+    rate:"35rs";
+    pages:90;
+    brand:"camel";
+    quality:"tnpl papers";
+
+}
+var idly;{
+    shape: "eclipse";
+    rate:6rs;
+    type:"steam cook";
+    color: "white";
+    increatiend:"rice";
+}
+// =========================15
+var moon;{
+    color:"white and black";
+    shape: oval;
+    type: solid;
+    distance: 100000"km";
+    rotate: "no rotate";
+}
+var charger;{
+    color:"white";
+    type:"b-type";
+    vlotage:24v;
+    length:"one-meter";
+    data-cable:"removable";
+}
+var shirt;{
+    color:"black";
+    type:"Cotton";
+    hand:"half-hand";
+    size:"localStorage";
+    rate:"550rs";
+}
+var bike;{
+    brand:"hero";
+    model:"splender+";
+    year:2018;
+    rate:"61000rs";
+    max.speed: "95kmph";
+}
+var book;{
+  name:"thermoDynamics";
+  author:"kurmi";
+  rate:"750rs";
+  pages:550;
+  weight:"500gm";
+}
